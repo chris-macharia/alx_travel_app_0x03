0d75e674c77018fa73eaf6b84771aaad637f09d4
chris macharia <christophermaina003@gmail.com>
2025-01-27 23:19:49 +0300

    Fix the listings API

--- listings/views.py
+++ listings/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Listing, Booking
 from .serializers import ListingSerializer, BookingSerializer
 
@@ -11,5 +11,19 @@ class ListingListCreateView(generics.ListCreateAPIView):
 
 
 class BookingListCreateView(generics.ListCreateAPIView):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+class ListingViewSet(viewsets.ModelViewSet):
+    """
+    A ViewSet for performing CRUD operations on Listings.
+    """
+    queryset = Listing.objects.all()
+    serializer_class = ListingSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    """
+    A ViewSet for performing CRUD operations on Bookings.
+    """
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
